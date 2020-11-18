e6be44e48aeb9bb9a858953515441514181e4efe
mikael-lehmuskoski <mikael.lehmuskoski@metropolia.fi>
2020-11-18 11:29:54 +0200

    front: memory leak fix'd

--- front/src/components/Clock/Ticker.js
+++ front/src/components/Clock/Ticker.js
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-// TODO: stop updating if view not active!!! MEMORY LEAK!!!
 // ODOD: styling, lots and lots of styling
 const Ticker = () => {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setTimeout(() => {
+    const update = setInterval(() => {
       setTime(new Date());
     }, 1000);
-  });
+
+    return () => {
+      clearInterval(update);
+    };
+  }, []);
 
   const hours = time.getHours();
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
-
-  return <div className="Ticker">{`${hours}:${minutes}:${seconds}`}</div>;
+  return (
+    <div className="Ticker">
+      {`${hours.toString().length === 1 ? 0 + hours.toString() : hours}:${
+        minutes.toString().length === 1 ? 0 + minutes.toString() : minutes
+      }:${seconds.toString().length === 1 ? 0 + seconds.toString() : seconds}`}
+    </div>
+  );
 };
 
 export default Ticker;
