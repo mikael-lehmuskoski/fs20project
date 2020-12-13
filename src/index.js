5935d28ba2f86c1e2e8d079b4b18a30bd3222649
mikael-lehmuskoski <mikael.lehmuskoski@metropolia.fi>
2020-12-13 12:48:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,10 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.disable("x-powered-by");
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, '/../build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 mongoose.set("useFindAndModify", false);
