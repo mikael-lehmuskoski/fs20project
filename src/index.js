1087e385224b5fdb5885e6e73d14127d5c8f7205
mikael-lehmuskoski <mikael.lehmuskoski@metropolia.fi>
2020-12-13 12:54:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.disable("x-powered-by");
 app.use(express.static(path.join(__dirname, '/../build')));
 
-app.get('/*', (req, res) => {
+app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
